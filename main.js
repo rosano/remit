9d2a8e595d83c6e652a4e7eddb80792a3cb2a0b6
Rosano Coutinho <github.com@xeari.com>
2024-03-28 16:26:30 -0400

    throw if domain map not defined

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 const mod = {
 
 	async HandleRequest (req, res) {
-		return res.send('hello')
+		try {
+			const domainMap = mod.DataDomainMap();
+		} catch (error) {
+			res.statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+
+			return res.send(error.message);
+		}
 	},
 
 	DataDomainMap() {
+		if (!process.env.REMIT_DOMAIN_MAP) {
+			throw new Error('REMIT_DOMAIN_MAP not defined');
+		}
+
 		return JSON.parse(process.env.REMIT_DOMAIN_MAP);
 	},
 
