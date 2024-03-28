71b3e26f35b6a3991f7481a2042d824986b253e1
Rosano Coutinho <github.com@xeari.com>
2024-03-28 16:20:58 -0400

    listen and handle all requests

--- main.js
+++ main.js
@@ -1,9 +1,27 @@
 const mod = {
 
+	async HandleRequest (req, res) {
+		return res.send('hello')
+	},
+
 	DataDomainMap() {
 		return JSON.parse(process.env.REMIT_DOMAIN_MAP);
 	},
 
+	// LIFECYCLE
+
+	LifecycleDidLoad () {
+		const app = require('express')();
+
+		app.get('*', mod.HandleRequest);
+
+		app.listen(parseInt(process.env.PORT || 3000));
+	},
+
+};
+
+if (process.env.npm_lifecycle_script === 'node main.js') {
+	mod.LifecycleDidLoad();
 };
 
 Object.assign(exports, mod);
