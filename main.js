cf7381a03762235178de7addf84863aa89ce9860
Rosano Coutinho <github.com@xeari.com>
2025-07-29 10:06:06 +0100

    download extensions as file

--- main.js
+++ main.js
@@ -12,6 +12,20 @@ app.use(async (req, res) => {
 
 	const source = base + req.host + req.path;
 
+	const extension = source.match(/\.(\w+)$/);
+
+	if (extension && (extension[1] !== 'html')) {
+		const destination = require('path').join(__dirname, '__download', require('crypto').createHash('md5').update(source).digest('hex') + '.' + extension[1]);
+
+		if (!require('fs').existsSync(require('path').dirname(destination))){
+			require('fs').mkdirSync(require('path').dirname(destination));
+		}
+
+		await require('util').promisify(require('stream').pipeline)((await fetch(source)).body, require('fs').createWriteStream(destination));
+
+		return res.sendFile(destination);
+	}
+
 	const response = await fetch(source);
 
 	return res.send(await response.text())
